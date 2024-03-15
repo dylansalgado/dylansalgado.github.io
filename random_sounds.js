85912634cf808fa4e49ce542dfd1a83adc72c3d0
dylansalgado <61199166+dylansalgado@users.noreply.github.com>
2024-03-14 20:25:37 -0500

    Update random_sounds.js

--- random_sounds.js
+++ random_sounds.js
@@ -1,9 +1,27 @@
 // Array containing paths to sound files
 var soundFiles = [
-  'Media/Fun Projects/Random Noises/sound1.mp3',
-  'Media/Fun Projects/Random Noises/sound2.mp3',
-  'Media/Fun Projects/Random Noises/sound3.mp3',
-  // Add more sound
+  'Media/Fun Projects/Random Noises/among-us-role-reveal-sound.mp3',
+  'Media/Fun Projects/Random Noises/anime-wow-sound-effect.mp3',
+  'Media/Fun Projects/Random Noises/bad-to-the-bone.mp3',
+  'Media/Fun Projects/Random Noises/bruh.mp3',
+  'Media/Fun Projects/Random Noises/cave.mp3',
+  'Media/Fun Projects/Random Noises/clash-royale-hog-rider.mp3',
+  'Media/Fun Projects/Random Noises/classic-hurt.mp3',
+  'Media/Fun Projects/Random Noises/cotton-eye-joe-nuggets.mp3',
+  'Media/Fun Projects/Random Noises/creeper-explosion.mp3',
+  'Media/Fun Projects/Random Noises/discord-notification.mp3',
+  'Media/Fun Projects/Random Noises/downer-noise.mp3',
+  'Media/Fun Projects/Random Noises/error.mp3',
+  'Media/Fun Projects/Random Noises/gnome-woo-reverb.mp3',
+  'Media/Fun Projects/Random Noises/goofy-ahh-car-horn-sound-effect.mp3',
+  'Media/Fun Projects/Random Noises/jixaw-metal-pipe-falling-sound.mp3',
+  'Media/Fun Projects/Random Noises/minecraft-villager-sound-effect.mp3',
+  'Media/Fun Projects/Random Noises/pikmin-gcn.mp3',
+  'Media/Fun Projects/Random Noises/roblox-death-sound.mp3',
+  'Media/Fun Projects/Random Noises/skibidi-toilet.mp3',
+  'Media/Fun Projects/Random Noises/vine-boom.mp3',
+  'Media/Fun Projects/Random Noises/wrong-answer-sound-effect.mp3',
+  // Add more sound eventually
 ];
 
 // Function to play a random sound from the soundFiles array
